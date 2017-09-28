4b7c8885afdc84cb0b2f149e473fea97fff21dc4
Hoai Thuong <thuong.hth@neo-lab.vn>
2017-09-28 10:17:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Test</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/custom.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="header">
+		<div class="container">
+			<div class="header-component">
+				<div class="logo">
+					<img src="img/Front-End-Test_03.png" alt="">
+				</div>
+				<div class="search-component">
+					<input type="text">
+					<button>Search</button>
+				</div>
+				<div class="cart-group">
+					<div class="cart-img">
+						<img src="img/Front-End-Test_05.png" alt="">
+					</div>
+					<div class="price">
+						$300
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="body">
+		<div class="nav-bar1">
+			<div class="container">
+				<ul>
+					<li><a class="item" href="">Home</a></li>
+					<li><a class="item"href="">Sale</a></li>
+					<li><a class="item" href="">Handbags</a></li>
+					<li><a class="item" href="">Wallets</a></li>
+					<li><a class="item" href="">Accessories</a></li>
+					<li><a class="item" href="">Menstore</a></li>
+					<li><a class="item" href="">Shoes</a></li>
+					<li><a class="item" href="">Vintage</a></li>
+					<li><a class="item" href="">Services</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="info">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+			<!-- 	<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol> -->
+
+				<!-- Wrapper for slides -->
+
+				<div class="carousel-inner" style="background-color: #3EC1B7;">
+					<div class="item active" style="background-color: #3EC1B7;">
+						<div class="img-view" style="background-color: #3EC1B7;">
+							<img src="img/Front-End-Test_10.png" alt="">
+						</div>
+						<div class="info-details">
+							<h1>COWHIDE STANDARD CREW</h1>
+							<p>White coloured, short-sleeved, printed T-shirt for men by Levi's. This crew-neck T-shirt is made of or-ganic cotton and comes in a regular fit.</p>
+							<button>SHOP NOW</button>
+						</div>
+					</div>
+
+					<div class="item" style="background-color: #3EC1B7; width: 100%;">
+						<div class="img-view">
+							<img src="img/Front-End-Test_14.png" alt="">
+						</div>
+						<div class="info-details">
+							<h1>COWHIDE STANDARD CREW</h1>
+							<p>White coloured, short-sleeved, printed T-shirt for men by Levi's. This crew-neck T-shirt is made of or-ganic cotton and comes in a regular fit.</p>
+							<button>SHOP NOW</button>
+						</div>
+					</div>
+
+					<div class="item" style="background-color: #3EC1B7;">
+						<div class="img-view">
+							<img src="img/Front-End-Test_16.png" alt="">
+						</div>
+						<div class="info-details">
+							<h1>COWHIDE STANDARD CREW</h1>
+							<p>White coloured, short-sleeved, printed T-shirt for men by Levi's. This crew-neck T-shirt is made of or-ganic cotton and comes in a regular fit.</p>
+							<button>SHOP NOW</button>
+						</div>
+					</div>
+				</div>
+
+				<!-- Left and right controls -->
+				<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#myCarousel" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
+			<!-- 	
+				<div class="img-view">
+					<img src="img/shirts-1.png" alt="">
+				</div>
+				<div class="info-details">
+					<h1>COWHIDE STANDARD CREW</h1>
+					<p>White coloured, short-sleeved, printed T-shirt for men by Levi's. This crew-neck T-shirt is made of or-ganic cotton and comes in a regular fit.</p>
+					<button>SHOP NOW</button>
+				</div> -->
+
+			</div>
+			<div class="branded-group">
+				<div class="container">
+					<div class="branded-item">
+						<div class="img-view">
+							<img src="img/Front-End-Test_14.png" alt="">
+						</div>
+						<div class="info-details">
+							<div class="title">
+								BRANDED SHOES
+							</div>
+							<button>
+								SHOP
+							</button>
+						</div>
+					</div>
+					<div class="branded-item">
+						<div class="img-view">
+							<img src="img/Front-End-Test_16.png" alt="">
+						</div>
+						<div class="info-details">
+							<div class="title">
+								BRANDED SHOES
+							</div>
+							<button>
+								SHOP
+							</button>
+						</div>
+					</div>
+					<div class="branded-item">
+						<div class="img-view">
+							<img src="img/Front-End-Test_18.png" alt="">
+						</div>
+						<div class="info-details">
+							<div class="title">
+								BRANDED SHOES
+							</div>
+							<button>
+								SHOP
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="feature-products">
+				<div class="container">
+					FEATURE PRODUCTS
+				</div>
+			</div>
+			<div class="branded-more">
+				<div class="container">
+					<div class="branded-item">
+						<div class="img-view">
+							<img src="img/Front-End-Test_24.png" alt="">
+						</div>
+						<div class="info-details">
+							<div class="title">BRANDED SHOES</div>						
+							<div class="group">
+								<div class="price">
+									$300
+								</div>
+								<button>
+									BUY NOW
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="branded-item">
+						<div class="img-view">
+							<img src="img/Front-End-Test_26.png" alt="">
+						</div>
+						<div class="info-details">
+							<div class="title">BRANDED SHOES</div>						
+							<div class="group">
+								<div class="price">
+									$300
+								</div>
+								<button>
+									BUY NOW
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="branded-item">
+						<div class="img-view">
+							<img src="img/Front-End-Test_28.png" alt="">
+						</div>
+						<div class="info-details">
+							<div class="title">BRANDED SHOES</div>						
+							<div class="group">
+								<div class="price">
+									$300
+								</div>
+								<button>
+									BUY NOW
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="branded-item">
+						<div class="img-view">
+							<img src="img/Front-End-Test_34.png" alt="">
+						</div>
+						<div class="info-details">
+							<div class="title">BRANDED SHOES</div>						
+							<div class="group">
+								<div class="price">
+									$300
+								</div>
+								<button>
+									BUY NOW
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="branded-item">
+						<div class="img-view">
+							<img src="img/Front-End-Test_36.png" alt="">
+						</div>
+						<div class="info-details">
+							<div class="title">BRANDED SHOES</div>						
+							<div class="group">
+								<div class="price">
+									$300
+								</div>
+								<button>
+									BUY NOW
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="branded-item">
+						<div class="img-view">
+							<img src="img/Front-End-Test_37.png" alt="">
+						</div>
+						<div class="info-details">
+							<div class="title">BRANDED SHOES</div>						
+							<div class="group">
+								<div class="price">
+									$300
+								</div>
+								<button>
+									BUY NOW
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<div class="linking-group">
+				<div class="container">
+					<div class="linking-item">
+						<div class="title">
+							FETURED SALE
+						</div>
+						<div class="link-detail">Alexis Hudson</div>
+						<div class="link-detail">American Apperel</div>
+						<div class="link-detail">Ben Sherman</div>
+						<div class="link-detail">Big Buddha</div>
+						<div class="link-detail">Channel</div>
+						<div class="link-detail">Christian Audigier</div>
+						<div class="link-detail">Coach</div>
+						<div class="link-detail">Cole Haan</div>
+					</div>
+					<div class="linking-item">
+						<div class="title">
+							MENS STORE
+						</div>
+						<div class="link-detail">Alexis Hudson</div>
+						<div class="link-detail">American Apperel</div>
+						<div class="link-detail">Ben Sherman</div>
+						<div class="link-detail">Big Buddha</div>
+						<div class="link-detail">Channel</div>
+						<div class="link-detail">Christian Audigier</div>
+						<div class="link-detail">Coach</div>
+						<div class="link-detail">Cole Haan</div>
+					</div>
+					<div class="linking-item">
+						<div class="title">
+							WOMEN STORE
+						</div>
+						<div class="link-detail">Alexis Hudson</div>
+						<div class="link-detail">American Apperel</div>
+						<div class="link-detail">Ben Sherman</div>
+						<div class="link-detail">Big Buddha</div>
+						<div class="link-detail">Channel</div>
+						<div class="link-detail">Christian Audigier</div>
+						<div class="link-detail">Coach</div>
+						<div class="link-detail">Cole Haan</div>
+					</div>
+					<div class="linking-item">
+						<div class="title">
+							QUICK LINKS
+						</div>
+						<div class="link-detail">Alexis Hudson</div>
+						<div class="link-detail">American Apperel</div>
+						<div class="link-detail">Ben Sherman</div>
+						<div class="link-detail">Big Buddha</div>
+						<div class="link-detail">Channel</div>
+						<div class="link-detail">Christian Audigier</div>
+						<div class="link-detail">Coach</div>
+						<div class="link-detail">Cole Haan</div>
+					</div>
+				</div>
+			</div>
+			<div class="copy-right">
+				<div class="container">
+					Copyright &copy; 2013 Css Author
+				</div>
+			</div>
+		</div>
+
+
+	</body>
+	</html>
+	<script src="js/javascript.js"></script>
+
